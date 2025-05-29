6baa8c088443bda51e1ec7528b398d4a5205edc2
blackcat <123@fox.mail>
2025-05-29 17:00:19 +0800

    完成首页和路由配置

---
+++ src/pages/door/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "@tarojs/components";
+
+import "./index.scss";
+
+const Door = () => {
+    return <View className="door">Door</View>;
+};
+
+export default Door;
